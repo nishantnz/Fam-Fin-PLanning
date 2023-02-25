b2f19f3cdce1bea6aefc3c95dfdd739f117d547e
nishantnz <nishant.z@somaiya.edu>
2023-02-26 03:01:38 +0530

    themes page

---
+++ lib/widgets/themes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // appBarTheme: AppBarTheme(
+        //   color: Colors.white,
+        //   elevation: 0,
+        //   //  iconTheme: const IconThemeData(color: Colors.black),
+        //   //toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
+        //   titleTextStyle: Theme.of(context).textTheme.headline6,
+        // ),
+      );
+
+  static ThemeData darkTheme(BuildContext context) =>
+      ThemeData(brightness: Brightness.dark);
+}
