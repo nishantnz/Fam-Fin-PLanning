9c114c85a08a1184dd4232dfb82b96b63f4713fc
nishantnz <nishant.z@somaiya.edu>
2023-02-26 23:40:58 +0530

    added signup page

---
+++ lib/pages/sign_up.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("data"),
+    );
+  }
+}
