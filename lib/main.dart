f467e475e33fcf4cb549196702dccae96ae005aa
nishantnz <nishant.z@somaiya.edu>
2023-02-26 23:39:27 +0530

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:family_financial_planning/pages/login.dart';
 import 'package:family_financial_planning/pages/onboarding_page.dart';
+import 'package:family_financial_planning/pages/sign_up.dart';
+import 'package:family_financial_planning/utils/routes.dart';
 import 'package:family_financial_planning/widgets/themes.dart';
 
 import 'package:flutter/material.dart';
@@ -16,13 +19,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: OnBoarding(),
-      title: 'Flutter Demo',
+      title: 'FFL',
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: MyTheme.lightTheme(context),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+      initialRoute: MyRoutes.onBoarding,
+      routes: {
+        MyRoutes.loginPage: (context) => LoginPage(),
+        //MyRoutes.homePage: (context) => HomePage(),
+        MyRoutes.signUpPage: (context) => SignUpPage(),
+        MyRoutes.loginPage: (context) => LoginPage(),
+        MyRoutes.onBoarding: (context) => OnBoarding(),
+      },
     );
   }
 }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+class MyRoutes {
+  static String loginPage = "/login";
+  static String homePage = "/home";
+  static String signUpPage = "/signUp";
+  static String onBoarding = "/onBoarding";
+}
