f59639b69ec8276cec9e175a31b8cd3a19ea00ee
nishantnz <nishant.z@somaiya.edu>
2023-02-27 05:08:49 +0530

    made login form with validation and lock animation

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,10 @@
+import 'package:family_financial_planning/utils/my_textFiled.dart';
+import 'package:family_financial_planning/utils/routes.dart';
 import 'package:family_financial_planning/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,6 +14,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final usernameController = new TextEditingController();
+  final passwordController = new TextEditingController();
+  bool animator = false;
+  final _formKey = GlobalKey<FormState>();
   final gradientColor = [
     const Color.fromRGBO(68, 36, 137, 1),
     const Color.fromRGBO(19, 11, 116, 1),
@@ -21,53 +28,120 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Center(
-          child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              //logo
-              const SizedBox(
-                height: 50,
-              ),
-              const Icon(
-                Icons.lock,
-                size: 100,
-              ),
-
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                "Welcome back!",
-                style: TextStyle(fontSize: 16),
-              ),
-              //username field
-              const SizedBox(
-                height: 50,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  //logo
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: Lottie.asset(
+                      "assets/files/lock2.json",
+                      animate: animator,
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  const Text(
+                    "Welcome back!",
+                    style: TextStyle(
+                      fontSize: 16,
                     ),
-                    fillColor: Colors.grey.shade200,
-                    filled: true,
-                    hintStyle: TextStyle(color: Colors.grey[500])),
+                  ),
+                  //username field
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  MyTextField(
+                    hintText: "Username",
+                    obscureText: false,
+                    controller: usernameController,
+                    formKey: _formKey,
+                  ),
+
+                  SizedBox(
+                    height: 30,
+                  ),
+                  //password field
+                  MyTextField(
+                      hintText: "Password",
+                      obscureText: true,
+                      controller: passwordController,
+                      formKey: _formKey),
+                  //forgot passwprd
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Forgot Password ?",
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.blueAccent),
+                        ),
+                      ],
+                    ),
+                  ),
+                  //sign in button
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      //Navigator.pushNamed(context, "/home");
+
+                      if (_formKey.currentState!.validate()) {
+                        setState(() {
+                          animator = true;
+                        });
+                        await Future.delayed(Duration(seconds: 4));
+                        Navigator.pushNamed(context, MyRoutes.homePage);
+                      }
+                    },
+                    child: const Text("Sign In"),
+                    style: TextButton.styleFrom(
+                      shadowColor: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      elevation: 10,
+                      backgroundColor: Colors.blueAccent,
+                      minimumSize: const Size(150, 50),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  //not a member ? sign up
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Not a member ?"),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Sign Up",
+                          style: TextStyle(color: Colors.blueAccent),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-
-              //password field
-
-              //forgot passwprd
-
-              //sign in button
-
-              //not a member ? sign in
-            ],
+            ),
           ),
         ),
       ),
