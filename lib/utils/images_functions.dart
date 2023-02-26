cc8d66b5f626be1927893b7c2c577734f328295f
nishantnz <nishant.z@somaiya.edu>
2023-02-27 05:12:19 +0530

    commenting out

--- lib/utils/images_functions.dart
+++ lib/utils/images_functions.dart
@@ -18,40 +18,33 @@ class AppLogo {
   }
 }
 
-class backGroundImage {
-  buildBackgroundImageBlur() {
-    return ClipRect(
-      child: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://cdn.dribbble.com/userupload/4441128/file/original-9c06ffe250f2d6c5199b20b401b3cd00.png?compress=1&resize=1200x900"),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-              child: Container(
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ),
-          ),
-          const Positioned(
-            bottom: 30,
-            right: 5,
-            height: 150,
-            width: 300,
-            child: Text(
-              "We help you plan a  financially stable and secure future for you & your loved ones.",
-              style: TextStyle(fontSize: 24, color: Colors.white),
-            ),
-          )
-        ],
-      ),
-    );
-  }
+// class backGroundImage {
+//   buildBackgroundImageBlur() {
+//     return ClipRect(
+//       child: Stack(
+//         children: [
+//           Container(
+//             decoration: const BoxDecoration(
+//               image: DecorationImage(
+//                 image: AssetImage("assets/files/money.json"),
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//           ),
+//           const Positioned(
+//             bottom: 30,
+//             right: 5,
+//             height: 150,
+//             width: 300,
+//             child: Text(
+//               "We help you plan a  financially stable and secure future for you & your loved ones.",
+//               style: TextStyle(fontSize: 24, color: Colors.white),
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
 
   // backGroundImageThree() {
   //   return Stack(
@@ -73,4 +66,4 @@ class backGroundImage {
   //     ],
   //   );
   // }
-}
+
