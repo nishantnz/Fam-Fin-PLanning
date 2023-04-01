57f6ec6f6f43400edec48e0d2907b2130d07bcc1
nishantnz <nishant.z@somaiya.edu>
2023-04-01 19:33:20 +0530

    added more widgets to home page , refactored code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:family_financial_planning/utils/colors.dart';
 import 'package:family_financial_planning/widgets/bottomNavBar.dart';
-import 'package:family_financial_planning/widgets/expense_tacker_card.dart';
+import 'package:family_financial_planning/widgets/my_home_widgets.dart';
 import 'package:family_financial_planning/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -18,7 +18,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    double percentage = 0.74;
     return SafeArea(
       right: true,
       left: true,
@@ -38,36 +37,9 @@ class _HomePageState extends State<HomePage> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              MyExpenseTrackerCard.expenseCard(),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  // Add an empty container to push the indicator to the right
-                  Padding(
-                    padding: const EdgeInsets.only(right: 15),
-                    child: CircularPercentIndicator(
-                      // animateFromLastPercent: true,
-                      animation: true,
-                      animationDuration: 500,
-                      circularStrokeCap: CircularStrokeCap.round,
-                      // restartAnimation: true,
-                      radius: 80,
-                      lineWidth: 20,
-                      percent: percentage,
-                      linearGradient: const LinearGradient(
-                          colors: MyColors.percentageGradientColor),
-                      center: Text(
-                        ((percentage * 100).toStringAsFixed(0)) + "%",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              MyHomeWidgets.expenseCard(),
+              MyHomeWidgets.goalProgress(),
+              // SizedBox(height: 20),
             ],
           ),
         ),
