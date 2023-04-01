89a4fbf7a9733fb884fa21b0fa9ff84427745b8a
nishantnz <nishant.z@somaiya.edu>
2023-04-01 15:42:57 +0530

    made partial home page and divided wigdets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,12 @@
+import 'package:family_financial_planning/utils/colors.dart';
+import 'package:family_financial_planning/widgets/bottomNavBar.dart';
+import 'package:family_financial_planning/widgets/expense_tacker_card.dart';
+import 'package:family_financial_planning/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,9 +18,59 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text("Home Page"),
+    double percentage = 0.74;
+    return SafeArea(
+      right: true,
+      left: true,
+      top: true,
+      bottom: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: GradientText(
+            "FINFAM",
+            colors: MyColors.appbartitlecolor,
+            gradientType: GradientType.linear,
+            style: const TextStyle(fontSize: 32),
+          ),
+          leading: Image.asset("assets/images/logo.jpg"),
+        ),
+        bottomNavigationBar: MyNavBarFul(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              MyExpenseTrackerCard.expenseCard(),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  // Add an empty container to push the indicator to the right
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: CircularPercentIndicator(
+                      // animateFromLastPercent: true,
+                      animation: true,
+                      animationDuration: 500,
+                      circularStrokeCap: CircularStrokeCap.round,
+                      // restartAnimation: true,
+                      radius: 80,
+                      lineWidth: 20,
+                      percent: percentage,
+                      linearGradient: const LinearGradient(
+                          colors: MyColors.percentageGradientColor),
+                      center: Text(
+                        ((percentage * 100).toStringAsFixed(0)) + "%",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/bottomNavBar.dart
@@ -0,0 +1,131 @@
+import 'package:family_financial_planning/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+class MyNavBarFul extends StatefulWidget {
+  const MyNavBarFul({Key? key});
+
+  @override
+  State<MyNavBarFul> createState() => _MyNavBarFulState();
+}
+
+class _MyNavBarFulState extends State<MyNavBarFul> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Home",
+          backgroundColor: Colors.indigo,
+          tooltip: "Home Page",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.text_snippet_outlined),
+          label: "Financial Details",
+          backgroundColor: Colors.indigo,
+          tooltip:
+              "Enter Your Financial Details and get to know more about plans of Family",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.monetization_on_outlined),
+          label: "Goals",
+          backgroundColor: Colors.indigo,
+          tooltip: "Get to know your Goals! ",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_sharp),
+          label: "Profile",
+          backgroundColor: Colors.indigo,
+          tooltip: "Profile Page",
+        ),
+      ],
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+          // Navigate to a new page when a BottomNavigationBarItem is tapped
+          // switch (index) {
+          //   // case 0:
+          //   //   Navigator.push(
+          //   //     context,
+          //   //     MaterialPageRoute(builder: (context) => HomePage()),
+          //   //   );
+          //   //   break;
+          //   // case 1:
+          //   //   Navigator.push(
+          //   //     context,
+          //   //     MaterialPageRoute(builder: (context) => FinancialDetailsPage()),
+          //   //   );
+          //   //   break;
+          //   // case 2:
+          //   //   Navigator.push(
+          //   //     context,
+          //   //     MaterialPageRoute(builder: (context) => GoalsPage()),
+          //   //   );
+          //   //   break;
+          //   case 3:
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(builder: (context) => ProfilePage()),
+          //     );
+
+          //     break;
+          // }
+        });
+      },
+    );
+  }
+}
+
+// // Define your new pages as separate widgets
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Home Page"),
+//       ),
+//       body: Center(
+//         child: Text("This is the Home Page"),
+//       ),
+//     );
+//   }
+// }
+
+// class FinancialDetailsPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Financial Details"),
+//       ),
+//       body: Center(
+//         child: Text("This is the Financial Details Page"),
+//       ),
+//     );
+//   }
+// }
+
+// class GoalsPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Goals"),
+//       ),
+//       body: Center(
+//         child: Text("This is the Goals Page"),
+//       ),
+//     );
+//   }
+// }
+
+// class ProfilePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ProfilePage();
+//   }
+// }
