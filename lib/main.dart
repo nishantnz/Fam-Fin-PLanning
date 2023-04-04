76933239e656c1b5f44e94e2b30a024a444d009b
nishantnz <nishant.z@somaiya.edu>
2023-04-04 18:53:49 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:family_financial_planning/pages/home_page.dart';
 import 'package:family_financial_planning/pages/login.dart';
 import 'package:family_financial_planning/pages/onboarding_page.dart';
+import 'package:family_financial_planning/pages/navpages/profile_page.dart';
 import 'package:family_financial_planning/pages/sign_up.dart';
 import 'package:family_financial_planning/utils/routes.dart';
 import 'package:family_financial_planning/widgets/themes.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: OnBoarding(),
+      home: const OnBoarding(),
       title: 'FFL',
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
@@ -27,13 +28,13 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: MyRoutes.onBoarding,
+      initialRoute: MyRoutes.homePage,
       routes: {
-        MyRoutes.loginPage: (context) => LoginPage(),
-        //MyRoutes.homePage: (context) => HomePage(),
-        MyRoutes.signUpPage: (context) => SignUpPage(),
-        MyRoutes.homePage: (context) => HomePage(),
-        MyRoutes.onBoarding: (context) => OnBoarding(),
+        MyRoutes.loginPage: (context) => const LoginPage(),
+        MyRoutes.signUpPage: (context) => const SignUpPage(),
+        MyRoutes.profile: (context) => const ProfilePage(),
+        MyRoutes.homePage: (context) => const HomePage(),
+        MyRoutes.onBoarding: (context) => const OnBoarding(),
       },
     );
   }
