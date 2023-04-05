068fff70ae70cf650446921654aa0df8795bff96
nishantnz <nishant.z@somaiya.edu>
2023-04-05 23:15:13 +0530

    financila_input init

--- lib/pages/navpages/financial_inputs.dart
+++ lib/pages/navpages/financial_inputs.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FinancialInputs extends StatelessWidget {
-  const FinancialInputs({super.key});
+  FinancialInputs({Key? key});
 
+  List items = ["Income/Salary", "Debts/Loans", "Investment"];
+  List subTitles = [
+    "(Annual)",
+    "Tell us about your Loans",
+    "Tell us about the investments you make"
+  ];
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Financial Inputs"),
-    );
+    return Text("hi");
   }
 }
